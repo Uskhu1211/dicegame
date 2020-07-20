494eec098d61b664d5d04b7a7ede5fefb02fea7f
Uskhu <usukhbayarch1211@gmail.com>
2020-07-20 22:12:14 +0800

    shaachlo

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var activePlayer = 0;
 var score = [0, 0];
 //toglogchiin eeljindee tsugluulj bui onoog hadgalah huwisagch
 var roundScore = 0;
+var gameOver = false;
 // shoo ali talaaraa buusan esehiig hadgalah,(1-6) utgiig sanamsarguigeer awah huwisagch
 var diceNumber = Math.floor(Math.random() * 6) + 1;
 var diceDom = window.document.querySelector(".dice");
@@ -12,31 +13,37 @@ var diceDom = window.document.querySelector(".dice");
 // document.getElementById("score-1").textContent = 0;
 // document.getElementById("current-1").textContent = 0;
 // document.getElementById("current-0").textContent = 0;
-gameFinish();
+initGame();
 document.querySelector(".btn-roll").addEventListener("click", function () {
-  var diceNumber = Math.floor(Math.random() * 6) + 1;
-  diceDom.src = "dice-" + diceNumber + ".png";
-  if (diceNumber !== 1) {
-    diceDom.style.display = "block";
-    roundScore = roundScore + diceNumber;
-    document.getElementById("current-" + activePlayer).textContent = roundScore;
-  } else {
-    nextPlayer();
-  }
+  if (gameOver !== true) {
+    var diceNumber = Math.floor(Math.random() * 6) + 1;
+    diceDom.src = "dice-" + diceNumber + ".png";
+    if (diceNumber !== 1) {
+      diceDom.style.display = "block";
+      roundScore = roundScore + diceNumber;
+      document.getElementById(
+        "current-" + activePlayer
+      ).textContent = roundScore;
+    } else {
+      nextPlayer();
+    }
+  } else alert("game is over please click  " + "New game  " + "button");
 });
 document.querySelector(".btn-hold").addEventListener("click", function () {
-  score[activePlayer] = score[activePlayer] + roundScore;
-  document.getElementById("score-" + activePlayer).textContent =
-    score[activePlayer];
-  if (score[activePlayer] >= 20) {
-    alert("player" + (activePlayer + 1) + "goy shaala");
-    document.getElementById("name-" + activePlayer).textContent = "winner";
-    gameFinish();
-  } else {
-    nextPlayer();
-  }
+  if (gameOver !== true) {
+    score[activePlayer] = score[activePlayer] + roundScore;
+    document.getElementById("score-" + activePlayer).textContent =
+      score[activePlayer];
+    if (score[activePlayer] >= 6) {
+      //      alert("Gameover" + "  " + "player" + (activePlayer + 1) + "goy shaala");
+      document.getElementById("name-" + activePlayer).textContent = "winner";
+      gameOver = true;
+    } else {
+      nextPlayer();
+    }
+  } else alert("game is over please click  " + "New game  " + "button");
 });
-document.querySelector(".btn-new").addEventListener("click", gameFinish());
+document.querySelector(".btn-new").addEventListener("click", initGame);
 
 function nextPlayer() {
   roundScore = 0;
@@ -50,13 +57,14 @@ function nextPlayer() {
   }
   diceDom.style.display = "none";
 }
-function gameFinish() {
+function initGame() {
+  gameOver = false;
   diceDom.style.display = "none";
   activePlayer = 0;
   score = [0, 0];
   roundScore = 0;
   document.getElementById("name-" + activePlayer).textContent =
-    "Player" + activePlayer;
+    "Player" + (activePlayer + 1);
   document.querySelector(".player-0-panel").classList.add("active");
   document.querySelector(".player-1-panel").classList.remove("active");
   document.getElementById("score-0").textContent = 0;
